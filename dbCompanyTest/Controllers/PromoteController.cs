d853cfc9cf9d09e31e73e8395575797ccb6cd5ac
SkyLin8012 <99629426+SkyLin8012@users.noreply.github.com>
2023-03-13 15:24:03 +0800

    2023 03 23

---
+++ dbCompanyTest/Controllers/PromoteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Excel =Microsoft.Office.Interop.Excel;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace dbCompanyTest.Controllers
+{
+    public class PromoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WebGame()
+        {
+            return View();
+        }
+    }
+}
